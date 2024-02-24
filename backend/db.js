068c0ff278374aef0d8309bd7cf89fac82bc40ee
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-02-25 01:18:58 +0530

    readed data

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,23 @@
 const mongoose = require("mongoose");
 
 const mongoURI =
-  'mongodb+srv://bhavyagg90:1234@cluster0.rilcgx2.mongodb.net/?retryWrites=true&w=majority';
+  "mongodb+srv://bhavyagg90:1234@cluster0.rilcgx2.mongodb.net/GoFoodmern?retryWrites=true&w=majority";
 
-const connectToMongoDB = async () => {
+const mongoDB = async () => {
   try {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("MongoDB connection error:", error);
-    throw error; // Rethrow the error to handle it in the calling code if necessary
+
+    console.log("Connected to MongoDB");
+
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray();
+    console.log(data);
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
   }
 };
 
-module.exports = connectToMongoDB;
+module.exports = mongoDB;
--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,11 @@ const app = express();
 const port = 5000;
 const mongoDB = require("./db");
 
-mongoDB()
-  .then(() => {
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
-    });
+mongoDB();
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
