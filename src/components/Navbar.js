c9e1751abf57654e3286cafd7b8aa943ed1f3bd3
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-02-27 23:46:37 +0530

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,7 +33,7 @@ export default function Navbar() {
               </li>
               <li className="nav-item">
                 <Link className="nav-link" to="/createuser ">
-                  Signup
+                  Signu
                 </Link>
               </li>
             </ul>
