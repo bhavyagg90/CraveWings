d1365e622d9f88738a5d6d711743273dfa8cc8ad
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-03-03 23:11:06 +0530

    kk

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -31,4 +31,22 @@ router.post(
   }
 );
 
+
+router.post(
+  "/loginuser",  async (req, res) => {
+   let email= req.body.email;
+    try {
+      let userData = await User.findOne({email});
+      if(!userData){
+        return res.status(400).json({ errors: "Try logging with correct credentials" });
+      }
+
+      
+    } catch (error) {
+      console.log(error);
+      res.json({ success: false });
+    }
+  }
+);
+
 module.exports = router;
