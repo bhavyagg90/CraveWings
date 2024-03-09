5e8d5f8198dc2bc32171a7dc5bbc51fa1e996476
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-03-09 20:59:35 +0530

    d

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatchCart, useCart } from "./ContextReducer";
 
 export default function Card(props) {
+  let dispatch = useDispatchCart();
   let options = props.options;
   let priceOptions = Object.keys(options);
-  const handleAddToCart =()=>{
-    
-  }
+  const [qty, setQty] = useState(1);
+  const [size, setSize] = useState("");
+  const handleAddToCart = async () => {
+    await dispatch({
+      type: "ADD",
+      id: props.foodItem._id,
+      name: props.foodItem.name,
+      price: props.finalPrice,
+      qty: qty,
+      size: size,
+    });
+  };
 
   return (
     <div>
       <div className="card mt-3" style={{ width: "18rem", maxHeight: "360px" }}>
         <img
-          src={props.imgSrc}
+          src={props.foodItem.img}
           className="card-img-top"
           alt="..."
           style={{ height: "120px", objectFit: "fill" }}
         />
         <div className="card-body">
-          <h5 className="card-title">{props.foodName}</h5>
+          <h5 className="card-title">{props.foodItem.name}</h5>
           <div className="container w-100">
-            <select className="m-2 h-100 bg-success rounded">
+            <select className="m-2 h-100 bg-success rounded" onChange={(e)=> setQty(e.target.value)}>
               {Array.from(Array(6), (e, i) => {
                 return (
                   <option key={i + 1} value={i + 1}>
@@ -28,7 +39,7 @@ export default function Card(props) {
                 );
               })}
             </select>
-            <select className="m-2 h-100 bg-success rounded">
+            <select className="m-2 h-100 bg-success rounded" onChange={(e)=> setSize(e.target.value)}>
               {priceOptions.map((data) => {
                 return (
                   <option key={data} value={data}>
--- src/components/ContextReducer.js
+++ src/components/ContextReducer.js
@@ -1,10 +1,16 @@
-import React, { createContext, useReducer } from 'react'
+import React, { createContext, useContext, useReducer } from 'react'
 
 const CartStateContext = createContext();
 const CartDispatchContext = createContext();
 
 const reducer = (state,action)=>{
-
+  switch(action.type){
+    case "ADD":
+      return [...state,{id:action}]
+      
+      default:
+        console.log("Error in Reducer");
+  }
 }
 
 export const CartProvider = ({children})=>{
@@ -17,3 +23,6 @@ export const CartProvider = ({children})=>{
     </CartDispatchContext.Provider>
   )
 }
+
+export const useCart = ()=> useContext(CartStateContext);
+export const useDispatchCart = ()=> useContext(CartDispatchContext);
