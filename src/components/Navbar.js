1ce2c394809b6660d484d22cf1f724f6330adbdd
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-03-17 21:01:17 +0530

    ms

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Badge from "react-bootstrap/Badge";
 export default function Navbar() {
-
-  const navigate = useNavigate()
-  const handleLogout =()=>{
+  const navigate = useNavigate();
+  const handleLogout = () => {
     localStorage.removeItem("authToken");
-    navigate("/login")
-  }
+    navigate("/login");
+  };
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
@@ -64,8 +64,19 @@ export default function Navbar() {
               </div>
             ) : (
               <div>
-                <div className="btn bg-white text-success mx-2">My Cart</div>
-                <div className="btn bg-white text-danger mx-2" onClick={handleLogout}>Logout</div>
+                <div className="btn bg-white text-success mx-2">
+                  My Cart{" "}
+                  <Badge pill bg="danger">
+                    {" "}
+                    2
+                  </Badge>
+                </div>
+                <div
+                  className="btn bg-white text-danger mx-2"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </div>
               </div>
             )}
           </div>
