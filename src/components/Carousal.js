e2976cfff1d749bdc05c3544b55bfc6aa8a15c3f
bhavyagg90 <86619571+bhavyagg90@users.noreply.github.com>
2024-04-04 23:19:17 +0530

    Update Carousal.js

--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -5,7 +5,7 @@ export default function Carousal() {
     <div>
       <div
         id="carouselExampleFade"
-       //ghgff
+       
         className="carousel slide carousel-fade"
         data-bs-ride="carousel"
         style={{ objectFit: "fill !important" }}
