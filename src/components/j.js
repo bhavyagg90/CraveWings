16c04a3967e0f859e89e24b674147b966c8205c9
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-04-18 11:09:26 +0530

    kj

---
+++ src/components/j.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function j() {
+  return (
+    <div>
+       <bu <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>tton
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+    </div>
+  )
+}
