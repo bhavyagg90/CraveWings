82b9a62a064e14b602979443bef27d596874f7c7
Bhavya Gupta <86619571+bhavyagg90@users.noreply.github.com>
2024-04-21 21:47:28 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -14,8 +14,7 @@ export default function Cart() {
   // const handleRemove = (index)=>{
   //   console.log(index)
   //   dispatch({type:"REMOVE",index:index})
-  // }
-hhsuus
+  //
   // const handleCheckOut = async () => {
   //   let userEmail = localStorage.getItem("userEmail");
   //   // console.log(data,localStorage.getItem("userEmail"),new Date())
