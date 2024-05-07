5d6f5e99104582b0583a3e89eebf8aa163000b72
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-05-07 21:22:54 +0530

    ss

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import DeleteIcon from '@mui/icons-material/Delete';
 import { useCart, useDispatchCart } from '../components/ContextReducer';
-import trash from "../aasets/trash.svg"
+import trash from "../aasets/trash.png";
 export default function Cart() {
   let data = useCart();
   let dispatch = useDispatchCart();
@@ -11,31 +10,32 @@ export default function Cart() {
         <div className='m-5 w-100 text-center fs-3'>The Cart is Empty!</div>
       </div>
     )     }
-  const handleRemove = (index)=>{
-    console.log(index)
-    dispatch({type:"REMOVE",index:index})
+  // const handleRemove = (index)=>{
+  //   console.log(index)
+  //   dispatch({type:"REMOVE",index:index})
+  // }
 
-  const handleCheckOut = async () => {
-    let userEmail = localStorage.getItem("userEmail");
-    console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/auth/orderData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        order_data: data,
-        email: userEmail,
-        order_date: new Date().toDateString()
-      })
-    });
-    console.log("JSON RESPONSE:::::", response.status)
-    if (response.status === 200) {
-      dispatch({ type: "DROP" })
-    }
-  }
+  // const handleCheckOut = async () => {
+  //   let userEmail = localStorage.getItem("userEmail");
+  //   console.log(data,localStorage.getItem("userEmail"),new Date())
+  //   let response = await fetch("http://localhost:5000/api/auth/orderData", {
+  //     // credentials: 'include',
+  //     // Origin:"http://localhost:3000/login",
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       order_data: data,
+  //       email: userEmail,
+  //       order_date: new Date().toDateString()
+  //     })
+  //   });
+  //   console.log("JSON RESPONSE:::::", response.status)
+  //   if (response.status === 200) {
+  //     dispatch({ type: "DROP" })
+  //   }
+  // }
 
   let totalPrice = data.reduce((total, food) => total + food.price, 0)
   return (
@@ -63,9 +63,11 @@ export default function Cart() {
                 <td>{food.qty}</td>
                 <td>{food.size}</td>
                 <td>{food.price}</td>
-                <td ><button type='button' className='btn p-0'>
-                    <img src={trash} alt='delete' onClick={() =>{dispatch({type: "REMOVE", index: index})}}/>
-                  </button> </td></tr>
+                <td >
+                  <button type="button" className='btn p-0'>
+                    <img src={trash}  alt='delete' onClick={() =>{dispatch({type: "REMOVE", index: index})}}/>
+                  </button> 
+                  </td></tr>
             ))}
           </tbody>
         </table>
@@ -79,4 +81,5 @@ export default function Cart() {
 
     </div>
   )
+  
 }
