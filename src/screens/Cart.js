77e9c8d5c1d9b427336c6687c4d9e581610df5ae
Bhavya Gupta <86619571+bhavyagg90@users.noreply.github.com>
2024-05-10 20:53:46 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -10,32 +10,32 @@ export default function Cart() {
         <div className='m-5 w-100 text-center fs-3'>The Cart is Empty!</div>
       </div>
     )     }
-  // const handleRemove = (index)=>{
-  //   console.log(index)
-  //   dispatch({type:"REMOVE",index:index})
-  // }
+  const handleRemove = (index)=>{
+    console.log(index)
+    dispatch({type:"REMOVE",index:index})
+  }
 
-  // const handleCheckOut = async () => {
-  //   let userEmail = localStorage.getItem("userEmail");
-  //   console.log(data,localStorage.getItem("userEmail"),new Date())
-  //   let response = await fetch("http://localhost:5000/api/auth/orderData", {
-  //     // credentials: 'include',
-  //     // Origin:"http://localhost:3000/login",
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({
-  //       order_data: data,
-  //       email: userEmail,
-  //       order_date: new Date().toDateString()
-  //     })
-  //   });
-  //   console.log("JSON RESPONSE:::::", response.status)
-  //   if (response.status === 200) {
-  //     dispatch({ type: "DROP" })
-  //   }
-  // }
+  const handleCheckOut = async () => {
+    let userEmail = localStorage.getItem("userEmail");
+    console.log(data,localStorage.getItem("userEmail"),new Date())
+    let response = await fetch("http://localhost:5000/api/auth/orderData", {
+      // credentials: 'include',
+      // Origin:"http://localhost:3000/login",
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        order_data: data,
+        email: userEmail,
+        order_date: new Date().toDateString()
+      })
+    });
+    console.log("JSON RESPONSE:::::", response.status)
+    if (response.status === 200) {
+      dispatch({ type: "DROP" })
+    }
+  }
 
   let totalPrice = data.reduce((total, food) => total + food.price, 0)
   return (
