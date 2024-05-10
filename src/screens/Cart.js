3b80bf287ec049a102ad68965af51f80934e5e14
Bhavya Gupta <86619571+bhavyagg90@users.noreply.github.com>
2024-05-10 20:54:09 +0530

    Update Cart.js

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -15,27 +15,27 @@ export default function Cart() {
     dispatch({type:"REMOVE",index:index})
   }
 
-  const handleCheckOut = async () => {
-    let userEmail = localStorage.getItem("userEmail");
-    console.log(data,localStorage.getItem("userEmail"),new Date())
-    let response = await fetch("http://localhost:5000/api/auth/orderData", {
-      // credentials: 'include',
-      // Origin:"http://localhost:3000/login",
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        order_data: data,
-        email: userEmail,
-        order_date: new Date().toDateString()
-      })
-    });
-    console.log("JSON RESPONSE:::::", response.status)
-    if (response.status === 200) {
-      dispatch({ type: "DROP" })
-    }
-  }
+  // const handleCheckOut = async () => {
+  //   let userEmail = localStorage.getItem("userEmail");
+  //   console.log(data,localStorage.getItem("userEmail"),new Date())
+  //   let response = await fetch("http://localhost:5000/api/auth/orderData", {
+  //     // credentials: 'include',
+  //     // Origin:"http://localhost:3000/login",
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       order_data: data,
+  //       email: userEmail,
+  //       order_date: new Date().toDateString()
+  //     })
+  //   });
+  //   console.log("JSON RESPONSE:::::", response.status)
+  //   if (response.status === 200) {
+  //     dispatch({ type: "DROP" })
+  //   }
+  // }
 
   let totalPrice = data.reduce((total, food) => total + food.price, 0)
   return (
