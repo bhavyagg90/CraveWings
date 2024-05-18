279a8424574899d48992ecb4aa0983ca35d0b7bb
Bhavya Gupta <86619571+bhavyagg90@users.noreply.github.com>
2024-05-18 23:24:25 +0530

    Update Home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
-
+hjtd
 export default function Home() {
   const [foodCat, setFoodCat] = useState([]);
   const [foodItem, setFoodItem] = useState([]);
