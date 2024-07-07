52fc4895fadceee63a32fc4743b3ee873e3eb528
Bhavya Gupta <86619571+bhavyagg90@users.noreply.github.com>
2024-07-07 23:24:54 +0530

    Update Login.js

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-kjbiSDVlhSD.vsdbv
 export default function Login() {
   const [credentials, setCredentials] = useState({
     name: "",
