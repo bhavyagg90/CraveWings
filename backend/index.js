334b3b15b2ee028825e01a52aa73bf10635e28c1
bhavyagg90 <gbhavya18sliet@gmail.com>
2024-12-20 05:17:30 +0530

    some changes

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,10 @@ const port = 5000;
 const mongoDB = require("./db");
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
   res.header(
-     "Access-Control-Allow-Headers",
-     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
@@ -17,9 +17,9 @@ mongoDB();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-app.use(express.json())
-app.use('/api', require("./Routes/CreateUser"))
-app.use('/api', require("./Routes/DisplayData"))
+app.use(express.json());
+app.use("/api", require("./Routes/CreateUser"));
+app.use("/api", require("./Routes/DisplayData"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
